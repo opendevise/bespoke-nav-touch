df5ffe5f55c4654bf6c7db6fc2b48e25d5183797
Dan Allen <dan.j.allen@gmail.com>
2016-02-21 22:41:39 -0700

    consolidate code

--- dist/bespoke-nav-touch.js
+++ dist/bespoke-nav-touch.js
@@ -6,10 +6,10 @@
  */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.navtouch = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function(options) {
+module.exports = function(opts) {
   return function(deck) {
     var TS = 'touchstart', TM = 'touchmove', ADD = 'addEventListener', RM = 'removeEventListener',
-      opts = (options || {}), src = deck.parent, start = null, axis = 'page' + (opts.axis === 'y' ? 'Y' : 'X'),
+      src = deck.parent, start = null, axis = 'page' + ((opts = opts || {}).axis === 'y' ? 'Y' : 'X'),
       gap = typeof opts.threshold === 'number' ? opts.threshold : 50 / window.devicePixelRatio,
       onStart = function(e) { start = e.touches.length === 1 ? e.touches[0][axis] : null; },
       onMove = function(e, delta) {
